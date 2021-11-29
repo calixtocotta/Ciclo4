1280c93c413988f21db398b122fae02fef2f73f6
calixto cotta <ccottav14@gmail.com>
2021-11-29 18:20:26 -0500

    lombok

--- src/main/java/ciclo4/ciclo4/Modelo/clothe.java
+++ src/main/java/ciclo4/ciclo4/Modelo/clothe.java
@@ -2,9 +2,9 @@
 package ciclo4.ciclo4.Modelo;
 
 
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 
@@ -13,10 +13,10 @@ import org.springframework.data.annotation.Id;
  *
  * @author calix
  */
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
 @Document(collection = "clothe")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Clothe{
     @Id
     private String reference;
@@ -26,84 +26,6 @@ public class Clothe{
     private boolean availability = true;
     private double price;
     private int quantity;
-    private String photography;
-
-    public Clothe() {
-    }
-
-    public Clothe(String reference, String category, String size, String description, double price, int quantity, String photography) {
-        this.reference = reference;
-        this.category = category;
-        this.size = size;
-        this.description = description;
-        this.price = price;
-        this.quantity = quantity;
-        this.photography = photography;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isAvailability() {
-        return availability;
-    }
-
-    public void setAvailability(boolean availability) {
-        this.availability = availability;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getPhotography() {
-        return photography;
-    }
-
-    public void setPhotography(String photography) {
-        this.photography = photography;
-    }
-    
+    private String photography; 
         
 }
