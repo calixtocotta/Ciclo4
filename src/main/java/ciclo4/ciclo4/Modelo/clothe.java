b1c6ed3a8ab9aea2db11a038b4adb33233ef46f8
calixto cotta <ccottav14@gmail.com>
2021-11-29 20:59:01 -0500

    get and set manual

--- src/main/java/ciclo4/ciclo4/Modelo/clothe.java
+++ src/main/java/ciclo4/ciclo4/Modelo/clothe.java
@@ -16,11 +16,11 @@ import org.springframework.data.annotation.Id;
  * @author calix
  */
 @Document(collection = "clothe")
-@Data
+/*@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Setter
+@Setter*/
 public class Clothe{
     @Id
     private String reference;
@@ -31,5 +31,83 @@ public class Clothe{
     private double price;
     private int quantity;
     private String photography; 
-        
+
+    public Clothe() {
+    }
+
+    public Clothe(String reference, String category, String size, String description, double price, int quantity, String photography) {
+        this.reference = reference;
+        this.category = category;
+        this.size = size;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.photography = photography;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPhotography() {
+        return photography;
+    }
+
+    public void setPhotography(String photography) {
+        this.photography = photography;
+    }
+    
+    
 }
