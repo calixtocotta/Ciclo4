22df8b1a72ccfcfa9e378b304dfac51f7915213b
calixto cotta <ccottav14@gmail.com>
2021-11-29 22:09:52 -0500

    ultimos archivos

--- src/main/java/ciclo4/ciclo4/Ciclo4Application.java
+++ src/main/java/ciclo4/ciclo4/Ciclo4Application.java
@@ -1,13 +1,25 @@
 package ciclo4.ciclo4;
 
+import ciclo4.ciclo4.Interface.interfaceClothe;
+import ciclo4.ciclo4.Interface.interfaceUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ciclo4Application {
-
+public class Ciclo4Application implements CommandLineRunner {
+    @Autowired
+    private interfaceClothe interfaceClothe;
+    @Autowired
+    private interfaceUser interfaceUser;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo4Application.class, args);
 	}
-
+    @Override
+    public void run(String... args) throws Exception {
+        interfaceClothe.deleteAll();
+        interfaceUser.deleteAll();
+    }
 }
