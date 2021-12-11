0cd128fef3d7881201de231433ffa722f0c8d445
calixto cotta <ccottav14@gmail.com>
2021-12-11 18:59:51 -0500

    reto 3

--- src/main/java/ciclo4/ciclo4/Interface/interfaceClothe.java
+++ src/main/java/ciclo4/ciclo4/Interface/interfaceClothe.java
@@ -2,7 +2,6 @@ package ciclo4.ciclo4.Interface;
 
 
 import ciclo4.ciclo4.Modelo.clothe;
-import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface interfaceClothe extends MongoRepository<clothe, String> {
