a6a1f5990858478eacbc0eb2e3fa02a48e98f755
calixto cotta <ccottav14@gmail.com>
2021-12-11 19:39:06 -0500

    hibernate

--- src/main/java/ciclo4/ciclo4/Interface/interfaceClothe.java
+++ src/main/java/ciclo4/ciclo4/Interface/interfaceClothe.java
@@ -5,7 +5,6 @@ import ciclo4.ciclo4.Modelo.clothe;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface interfaceClothe extends MongoRepository<clothe, String> {
-    //Lo utilizo porque con findById siempre me devuelve null
-   //Optional<Clothe> findByReference(String reference);
+
     
 }
